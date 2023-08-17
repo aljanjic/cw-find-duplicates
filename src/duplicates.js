b4c9af551b8fc30aa25f8bdca71f62d5e693ef2e
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-08-17 21:03:30 +0200

    Checking different solutions

--- src/duplicates.js
+++ src/duplicates.js
@@ -1,5 +1,27 @@
-function duplicates() {
-  return 'duplicates';
+function duplicates(arr) {
+
+  // create an array of duplicates
+  let duplicates = arr.filter((item, i) => arr.includes(item, i + 1 ))
+  let uniq = [... new Set(duplicates)]
+  console.log(uniq)
+  
+
+
+  // return the array of duplicates
+  return uniq;
+
+
+  // const duplicates = arr => [...new Set(arr.filter((el, i) => i !== arr.indexOf(el)))];
+
+  // let x = [];
+  // let y = [];
+
+  // arr.forEach((i) => {
+  // 	if (x.includes(i) === false) return x.push(i);
+  // 	if (x.includes(i) && y.includes(i) === false) return y.push(i);
+  // })
+
+  // return y
 }
 
 module.exports = duplicates;
--- src/duplicates.test.js
+++ src/duplicates.test.js
@@ -2,7 +2,10 @@ const duplicates = require('./duplicates');
 
 describe('findDuplicates', () => {
   it('should return findDuplicates', () => {
-    expect(findDuplicates()).toBe('findDuplicates');
+    expect(duplicates([1, 2, 3, 3, 2, 1])).toBe('findDuplicates');
+  });
+  it('should return findDuplicates', () => {
+    expect(duplicates([1, 2, 3])).toBe('findDuplicates');
   });
 });
 
