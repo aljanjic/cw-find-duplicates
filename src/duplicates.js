5693b6df2fdf677c53793779d83876655e515500
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-08-21 10:55:29 +0200

    Preparing tests + Cleaning up unnecessary code

--- src/duplicates.js
+++ src/duplicates.js
@@ -1,27 +1,17 @@
 function duplicates(arr) {
 
-  // create an array of duplicates
-  let duplicates = arr.filter((item, i) => arr.includes(item, i + 1 ))
-  let uniq = [... new Set(duplicates)]
-  console.log(uniq)
-  
 
+  const duplicates = arr => [...new Set(arr.filter((el, i) => i !== arr.indexOf(el)))];
 
-  // return the array of duplicates
-  return uniq;
+  let x = [];
+  let y = [];
 
+  arr.forEach((i) => {
+  	if (x.includes(i) === false) return x.push(i);
+  	if (x.includes(i) && y.includes(i) === false) return y.push(i);
+  })
 
-  // const duplicates = arr => [...new Set(arr.filter((el, i) => i !== arr.indexOf(el)))];
-
-  // let x = [];
-  // let y = [];
-
-  // arr.forEach((i) => {
-  // 	if (x.includes(i) === false) return x.push(i);
-  // 	if (x.includes(i) && y.includes(i) === false) return y.push(i);
-  // })
-
-  // return y
+  return y
 }
 
 module.exports = duplicates;
--- src/duplicates.test.js
+++ src/duplicates.test.js
@@ -2,10 +2,10 @@ const duplicates = require('./duplicates');
 
 describe('findDuplicates', () => {
   it('should return findDuplicates', () => {
-    expect(duplicates([1, 2, 3, 3, 2, 1])).toBe('findDuplicates');
+    expect(duplicates([1, 2, 3, 3, 2, 1])).toStrictEqual([3,2,1]);
   });
   it('should return findDuplicates', () => {
-    expect(duplicates([1, 2, 3])).toBe('findDuplicates');
+    expect(duplicates([1, 2, 3, 4, 3])).toStrictEqual([3]);
   });
 });
 
