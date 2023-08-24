2d52b1e14f261d507433e9d66c9429614d75b940
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-08-24 08:52:52 +0200

    Recreating submited solution

--- src/duplicates.js
+++ src/duplicates.js
@@ -1,15 +1,43 @@
 function duplicates(arr) {
 
 
-  let x = [];
-  let y = [];
+//   let x = [];
+//   let y = [];
 
-  arr.forEach((i) => {
-  	if (x.includes(i) === false) return x.push(i);
-  	if (x.includes(i) && y.includes(i) === false) return y.push(i);
-  })
+//   arr.forEach((i) => {
+//   	if (x.includes(i) === false) return x.push(i);
+//   	if (x.includes(i) && y.includes(i) === false) return y.push(i);
+//   })
 
-  return y
+//   return y
+
+
+
+
+
+
+
+let firstEncounter = [];
+let duplicates = [];
+
+
+arr.forEach(el => {
+  
+  if(!firstEncounter.includes(el)) return firstEncounter.push(el)
+  if(firstEncounter.includes(el) && !duplicates.includes(el)) return duplicates.push(el)
+
+});
+
+return duplicates;
+
+
 }
+
+
+
+
+
+
+
 
 module.exports = duplicates;
